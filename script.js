ba84148f3b1a721ffcb785103c4b4e8778991b28
Austinjames99 <james.aj.austin@gmail.com>
2023-01-30 20:27:54 -0500

    Add key event listeners

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ let y = canvas.height - 30
 let dx = 2 
 let dy = -2
 const ballRadius = 10
+const paddleHeight = 10
+const paddleWidth = 75
+let paddleX = (canvas.width - paddleWidth) / 2
+let rightPressed = false
+let leftPressed = false
 
 
-// Move The Ball
+// Draw The Ball
 function drawBall() {
     ctx.beginPath()
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2)
@@ -18,27 +23,57 @@ function drawBall() {
     ctx.fill()
     ctx.closePath()
 }
+// Draw Paddle
+    function drawPaddle() {
+        ctx.beginPath()
+        ctx.rect(paddleX, canvas.height - paddleHeight, 
+                paddleWidth, paddleHeight)
+        ctx.fillStyle = '#0095DD'
+        ctx.fill()
+        ctx.closePath()
 
+    }
+// Move The Ball
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     drawBall()
     x += dx
     y += dy
  // Wall Collision
-    if (x + dx > canvas.width) {
+    if (x + dx > canvas.width - ballRadius) {
         dx = -dx
     }
-    if (x + dx < 0) {
+    if (x + dx < ballRadius) {
         dx = -dx
     }
-    if (y + dy < 0) {
+    if (y + dy < ballRadius) {
         dy = -dy
     }
-    if (y + dy > canvas.height) {
+    if (y + dy > canvas.height - ballRadius) {
         dy = -dy
     }
 
 }
+
+// Figure Out if keys are being pressed
+document.addEventListener("keydown", keyDownHandler, false)
+document.addEventListener("keyup", keyUpHandler, false)
+
+function keyDownHandler(e)
+ if (e.key === 'Right' || e.key === "ArrowRight") {
+    rightPressed = true
+ } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    leftPressed = true
+ }
+
+ function keyUpHandler(e) {
+  if (e.key === 'Right' || e.key === "ArrowRight") {
+     rightPressed = false
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+     leftPressed = false
+  }
+    
+ }
 
 setInterval(draw, 10)
 
